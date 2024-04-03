78bc94b77cab3ecfd438284a1cc65e47640f859c
Capeling <113705504+Capeling@users.noreply.github.com>
2024-04-03 21:17:24 +1030

    im a bigger hater

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,50 @@
 
 using namespace geode::prelude;
 
+class SoggyStuff : public CCMenu {
+protected:
+	CCSprite* m_sogSpr = nullptr;
+	CCMenuItemSpriteExtra* m_sogBtn = nullptr;
+public:
+	static SoggyStuff* create() {
+		auto ret = new SoggyStuff();
+		if(ret && ret->init()) {
+			ret->autorelease();
+			return ret;
+		}
+		CC_SAFE_DELETE(ret);
+		return ret;
+	}
+	bool init() {
+		if(!CCMenu::init())
+			return false;
+
+		auto director = CCDirector::sharedDirector();
+    	auto winSize = director->getWinSize();
+
+		m_sogSpr = CCSprite::createWithSpriteFrameName("GJ_soggyBtn_001.png"_spr);
+		m_sogBtn = CCMenuItemSpriteExtra::create(m_sogSpr, this, menu_selector(SoggyStuff::onSog));
+
+		m_sogBtn->m_animationType = MenuAnimationType::Move;
+		m_sogBtn->m_startPosition = m_sogSpr->getPosition();
+		m_sogBtn->m_destPosition = ccp(0, -15.f);
+		m_sogBtn->setPositionY(-2);
+		this->addChild(m_sogBtn);
+
+		scheduleUpdate();
+
+		return true;
+	}
+	virtual void update(float dt) {
+		m_sogBtn->setVisible(true);
+		m_sogSpr->setVisible(true);
+		setVisible(true);
+	}
+	void onSog(CCObject*) {
+		CCDirector::sharedDirector()->replaceScene(CCTransitionMoveInT::create(0.5f, SogLayer::scene()));
+	}
+};
+
 class $modify(GJGarageLayerExt, GJGarageLayer) {
 	bool init() {
 		if(!GJGarageLayer::init())
@@ -10,21 +54,12 @@ class $modify(GJGarageLayerExt, GJGarageLayer) {
 		auto director = CCDirector::sharedDirector();
     	auto winSize = director->getWinSize();
 
-		auto sogSpr = CCSprite::createWithSpriteFrameName("GJ_soggyBtn_001.png"_spr);
-		auto sogBtn = CCMenuItemSpriteExtra::create(sogSpr, this, menu_selector(GJGarageLayerExt::onSog));
-		
-		auto sogMenu = CCMenu::create();
-		sogMenu->setPosition(ccp((winSize.width / 2) + 141, director->getScreenTop() - 23));
-		sogMenu->addChild(sogBtn);
+		auto soggyStuff = SoggyStuff::create();
 
-		sogBtn->m_animationType = MenuAnimationType::Move;
-		sogBtn->m_startPosition = sogSpr->getPosition();
-		sogBtn->m_destPosition = ccp(0, -15.f);
-		sogBtn->setPositionY(-2);
-		this->addChild(sogMenu);
+		soggyStuff->setPosition(ccp((winSize.width / 2) + 141, director->getScreenTop() - 23));
+
+		this->addChild(soggyStuff);
+
 		return true;
-	}
-	void onSog(CCObject*) {
-		CCDirector::sharedDirector()->replaceScene(CCTransitionMoveInT::create(0.5f, SogLayer::scene()));
 	}
 };
