2a87a20647e9d85644b38b0617ed327792522821
Fire <17692105+FireMario211@users.noreply.github.com>
2025-08-09 04:45:15 +0000

    add public funcs because I forgor

--- include/Events.hpp
+++ include/Events.hpp
@@ -5,8 +5,13 @@
 namespace soggy_mod {
 
 class OnSogLayer : public geode::Event {
+protected:
+    SogLayer* m_layer;
+    int m_rand;
 public:
-    OnSogLayer(SogLayer* layer, int rand) {};
+    OnSogLayer(SogLayer* layer, int rand) : m_layer(layer), m_rand(rand) {};
+    SogLayer* getLayer() const { return m_layer; };
+    int getRandom() const { return m_rand; };
 };
 
 }
